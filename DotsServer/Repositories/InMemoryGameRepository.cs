b7d82afb146a1735960f8028c624519ba8033ef3
Jakub Wójtowicz <j.wojtowicz0105@gmail.com>
2025-12-02 19:11:51 +0100

    Update InMemoryGameRepository.cs
    Change private Dictionary to ConcurrentDictionary to be more thread-safe

--- DotsServer/Repositories/InMemoryGameRepository.cs
+++ DotsServer/Repositories/InMemoryGameRepository.cs
@@ -1,12 +1,21 @@
+using System.Collections.Concurrent;
 using DotsWebApi.Model;
 
 namespace DotsWebApi.Repositories;
 
 public class InMemoryGameRepository : IGameRepository
 {
-    private readonly Dictionary<string, GameState> _games = new();
-    public void Add(string gameId, GameState state) => _games[gameId] = state;
-    public GameState? Get(string gameId) => _games.TryGetValue(gameId, out var state) ? state : null;
-    public bool Exists(string gameId) => _games.ContainsKey(gameId);
-    public void Update(string gameId, GameState state) => _games[gameId] = state;
+    private readonly ConcurrentDictionary<string, GameState> _games = new();
+
+    public void Add(string gameId, GameState state)
+        => _games[gameId] = state; 
+
+    public GameState? Get(string gameId)
+        => _games.TryGetValue(gameId, out var state) ? state : null;
+
+    public bool Exists(string gameId)
+        => _games.ContainsKey(gameId);
+
+    public void Update(string gameId, GameState state)
+        => _games[gameId] = state; 
 }
